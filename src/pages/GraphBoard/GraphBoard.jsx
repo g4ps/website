1cf28a85ca5a66f6932bb72104317cf7da2288db
g4ps <eum4rckin@yandex.ru>
2023-07-21 11:23:02 +0000

    Added some stuff with scaling

--- src/pages/GraphBoard/GraphBoard.jsx
+++ src/pages/GraphBoard/GraphBoard.jsx
@@ -1,8 +1,37 @@
-import {useEffect, useState, useRef, useCallback} from 'react';
+import {useEffect, useState, useRef, useCallback, useMemo} from 'react';
 import { useSelector } from 'react-redux';
 import './GraphBoard.scss';
 
+const rad = 10;
+
+const notColliding = (arr, pos, rad) => {
+    for (let j = 0; j < arr.length; j++) {
+        let i = arr[j];
+        if (sq_norm([i.pos[0] - pos[0], i.pos[1] - pos[1]]) < (rad * 2) ** 2)
+            return false;
+    }
+    return true;
+};
+
+const sq_norm = (vec) => {
+    let ret = 0;
+    vec.map(i => ret += i ** 2);
+    return ret;
+};
+
+const movingHandling = (ctx, currentOffc, screenOffc) => {
+    if (currentOffc.current.toString() !== screenOffc.toString()) {
+        let k = currentOffc.current;
+        let j = screenOffc;
+        let ret = screenOffc.slice(0);
+        let diff = [screenOffc[0] - currentOffc.current[0],
+                    screenOffc[1] - currentOffc.current[1]];
+        ctx.translate(-diff[0], diff[1]);
+    }
+};
+
 const drawCirc = (ctx, start, rad = 30) => {
+    // debugger;
     ctx.beginPath();
     ctx.arc(start[0], start[1], rad, 0, Math.PI * 2, true); // Outer circle
     ctx.stroke();
@@ -18,7 +47,8 @@ const drawEdge = (ctx, start, end, rad=30) => {
     let endpos = [end[0] - nrm[0] * rad, end[1] - nrm[1] * rad];
     ctx.lineTo(endpos[0], endpos[1]);
     const arrowLen = rad / 2;
-    //Rotation matrix
+    //Here we essentially use a 2D rotation matrix to draw arrows.
+    //For explanation go to wiki or any good linear algebra book
     ctx.lineTo(endpos[0] - (nrm[0] * 0.866 * arrowLen) - (nrm[1] * 0.5 * arrowLen) ,
                endpos[1] + (nrm[0] * 0.5 * arrowLen) - (nrm[1] * 0.866 * arrowLen));
     ctx.lineTo(endpos[0] - (nrm[0] * 0.866) * arrowLen + (nrm[1] * 0.5) * arrowLen,
@@ -28,17 +58,17 @@ const drawEdge = (ctx, start, end, rad=30) => {
     ctx.fill();
 };
 
-const rad = 30;
+
 
 const GraphBoard = () => {
 
     const isDarkTheme = useSelector(state => state.theme.value);
-
-    const  back_color = isDarkTheme ? "#444" : "#eee";
+    const back_color = isDarkTheme ? "#444" : "#eee";
     const stroke_color = isDarkTheme ? "white" : "#444";
-    const callsRef = useRef([0, 0, 0, 0, 0, 0]);    
+    
     const canvasRef = useRef(null);
     const canvasOverRef = useRef(null);
+    
 
     const graphRef = useRef({
         nodes: [],
@@ -48,89 +78,139 @@ const GraphBoard = () => {
     const prevNode = useRef(null);
     const keyRef = useRef([]);
     const mousePos = useRef(null);
+    const currentOffc = useRef([0, 0]);
+    const fieldMouseRef = useRef([0, 0]);
     const [mousePosState, setMousePosState] = useState(null);
-
     const [mouseDownPos, setMouseDownPos] = useState(null);
+    
     const [isMousePressed, setIsMousePressed] = useState(false);
     
     const [currNode, setCurrNode] = useState(null);
 
-    const [canvasRes, setCanvasRes] = useState([100, 100]);
+    const [canvasRes, setCanvasRes] = useState([500, 500]);
 
     const [keyPress, setKeyPress] = useState([]);
     const [graph, setGraph] = useState(null);
 
-    const [tempNodeID, setTempNodeID] = useState(null);
-    const [tempNodePos, setTempNodePos] = useState(null);
+    const [scale, setScale] = useState(1.0);
 
-    const sq_norm = (vec) => {
-        let ret = 0;
-        vec.map(i => ret += i ** 2);
-        return ret;
-    };
+    const [screenOffc, setScreenOffc] = useState([50, 50]);
 
-    useEffect(() => {
-        console.log(graph);
-    }, [graph]);
+    const fieldSize = useMemo(() => [[0, 2000], [0, 2000]], []);
 
-    const notColliding = useCallback((arr, pos, rad) => {
-        for (let j = 0; j < arr.length; j++) {
-            let i = arr[j];
-            if (sq_norm([i.pos[0] - pos[0], i.pos[1] - pos[1]]) < (rad * 2) ** 2)
-                return false;
+    const canvasFOV = useMemo(() => {
+        return [canvasRes[0] / scale, canvasRes[1] / scale];
+    }, [canvasRes, scale]);
+
+    const fieldToCanvas = useCallback((vec) => {
+        return [vec[0] - screenOffc[0], canvasRes[1] - vec[1] + screenOffc[1]];
+    }, [screenOffc, canvasRes]);
+
+    const canvasToField = useCallback((vec) => {
+        if (vec === null)
+            return null;
+        let sc = screenOffc;
+        let cr = canvasRes;
+        return [vec[0] / scale + screenOffc[0], canvasRes[1] - vec[1] / scale + screenOffc[1]];
+    }, [screenOffc, scale, canvasRes]);
+
+    const changeScreenOffc = useCallback((dx, dy) => {
+        let n = [screenOffc[0] + dx, screenOffc[1] + dy];
+        const setWithinBoundaries = (bound, arg) => {
+            for (let i = 0; i < 2; i++) {
+                if (arg[i] < bound[i][0]) {
+                    arg[i] = bound[i][0];
+                }
+                if (arg[i] > bound[i][1] - canvasFOV[i]) {
+                    arg[i] = bound[i][1] - canvasFOV[i];
+                }
+            }
+        };
+        setWithinBoundaries(fieldSize, n);
+        setScreenOffc(n);
+    }, [screenOffc, canvasFOV, fieldSize]);
+
+    const fieldMousePos = useMemo(() => {
+        if (!mousePosState || !screenOffc) {
+            return null;
         }
-        return true;
+        fieldMouseRef.current = canvasToField(mousePosState);
+        return fieldMouseRef.current;
+    }, [mousePosState, screenOffc, canvasToField]);
+
+    const clearEverything = useCallback(() => {
+        graphRef.current = {
+            nodes: [],
+            edges: []
+        };
+        setGraph(JSON.parse(JSON.stringify(graphRef.current)));
+        setCurrNode(null);
     }, []);
 
+    const addNode = useCallback((pos) => {
+        let currID = maxID.current;
+        maxID.current++;
+        graphRef.current.nodes.push({id: currID, pos: pos});
+        setGraph(JSON.parse(JSON.stringify(graphRef.current)));        
+    }, []);
+
+    const deleteNode = useCallback((id) => {
+        graphRef.current.nodes = graphRef.current.nodes.filter(i => i.id !== id);
+        graphRef.current.edges = graphRef.current.edges.filter(i => i.nodes.indexOf(id) < 0);
+        setGraph(JSON.parse(JSON.stringify(graphRef.current)));
+        setCurrNode(null);
+    }, []);
 
     useEffect(() => {
-        if (isMousePressed) {
-            if (currNode) {
-                setTempNodeID(currNode);
-                // console.log(currNode);
-            }
-        }
-        else {
-            setTempNodeID(null);
-            // console.log(null);
-        }
-    }, [isMousePressed, currNode]);
+        console.log(scale);
+    }, [scale]);
 
     const keyPressHandle = useCallback((key) => {
+        key = key.toLowerCase();
+        console.log(key);
         if (key === 'a') {
-            if (mousePos.current &&
-                notColliding(graphRef.current.nodes, mousePos.current, 30)) {
-                let currID = maxID.current;
-                maxID.current++;
-                graphRef.current.nodes.push({id: currID, pos: mousePos.current});
-                setGraph(JSON.parse(JSON.stringify(graphRef.current)));
+            if (fieldMouseRef.current &&
+                notColliding(graphRef.current.nodes, fieldMouseRef.current, rad)) {
+                addNode(fieldMouseRef.current);
             }
         }
         if (key === 'c') {
-            graphRef.current = {
-                nodes: [],
-                edges: []
-            };
-            setGraph(JSON.parse(JSON.stringify(graphRef.current)));
-            setCurrNode(null);
+            clearEverything();
         }
-        if (key === 'd') {
+        if (key === 'd' || key === 'delete') {
             if (currNode !== null) {
-                graphRef.current.nodes = graphRef.current.nodes.filter(i => i.id !== currNode);
-                graphRef.current.edges = graphRef.current.edges.filter(i => i.nodes.indexOf(currNode) < 0);
-                setGraph(JSON.parse(JSON.stringify(graphRef.current)));
-                setCurrNode(null);
+                deleteNode(currNode);
             }
         }
-    }, [notColliding, currNode]);
+        if (key === "arrowup") {
+            changeScreenOffc(0, 50); 
+        }
+        if (key === "arrowdown") {
+            changeScreenOffc(0, -50); 
+        }
+        if (key === "arrowleft") {
+            changeScreenOffc(-50, 0); 
+        }
+        if (key === "arrowright") {
+            changeScreenOffc(50, 0); 
+        }
+        if (key === "p") {
+            setScale(Math.min(3, scale + 0.1));
+        }
+        if (key === "l") {
+            setScale(Math.max(0.5, scale - 0.1));
+        }
+        if (key === "m") {
+            console.log(fieldMouseRef.current);
+        }
+    }, [currNode, changeScreenOffc, addNode,
+        canvasToField, clearEverything, deleteNode, fieldToCanvas, scale]);
 
-    const mouseDownHandle = useEffect(() => {
+    useEffect(() => {
         if (mouseDownPos !== null) {
-            callsRef.current[1]++;
             if (currNode !== null) {
-                let k = currNode;
-                if (notColliding(graphRef.current.nodes.filter(i => i.id !== currNode), mousePos.current, rad)) {
-                    graphRef.current.nodes.find(i => i.id === currNode).pos = mousePos.current;
+                if (notColliding(graphRef.current.nodes.filter(i => i.id !== currNode), fieldMouseRef.current, rad)) {
+                    graphRef.current.nodes.find(i => i.id === currNode).pos = fieldMouseRef.current;
                     setGraph(JSON.parse(JSON.stringify(graphRef.current)));
                     setCurrNode(null);
                     setMouseDownPos(null);
@@ -138,9 +218,11 @@ const GraphBoard = () => {
                 }
             }
             let set = false;
+
             for (let i = 0; i < graph?.nodes?.length; i++) {
                 let j = graph?.nodes[i];
-                if (sq_norm([j.pos[0] - mouseDownPos[0], j.pos[1] - mouseDownPos[1]]) <
+                let k = canvasToField(mouseDownPos);
+                if (sq_norm([j.pos[0] - k[0], j.pos[1] - k[1]]) <
                     rad ** 2) {
                     setCurrNode(j.id);
                     set = true;
@@ -150,16 +232,11 @@ const GraphBoard = () => {
                 setCurrNode(null);
             }
             setMouseDownPos(null);
-
         }
-    }, [currNode, mouseDownPos, graph]);
+    }, [currNode, mouseDownPos, graph, canvasToField]);
+
 
     useEffect(() => {
-        callsRef.current[2]++; 
-    }, [graph]);
-
-    useEffect(() => {
-        callsRef.current[3]++; 
         if (currNode === null) {
             prevNode.current = null;
             return ;
@@ -186,8 +263,7 @@ const GraphBoard = () => {
             else {
                 let ind = graphRef.current.edges.indexOf(curr_edge);
                 if (ind >= 0) {
-                    graphRef.current.edges.splice(ind, 1);
-                    
+                    graphRef.current.edges.splice(ind, 1);                    
                 }
                 setGraph(JSON.parse(JSON.stringify(graphRef.current)));
                 setCurrNode(null);
@@ -200,13 +276,14 @@ const GraphBoard = () => {
     }, [graph, currNode]);
 
     useEffect(() => {
-        callsRef.current[4]++;
         const canvas = canvasOverRef.current;
         if (!canvas)
             return () => {};
         
         const canvasResolutionSet = () => {
-            setCanvasRes([window.innerWidth, window.innerHeight - 60]);
+            let nr = [window.innerWidth, window.innerHeight - 60];
+            if (canvasRes.toString() !== nr.toString())
+                setCanvasRes(nr);
         };
         
         const keyDown = (e) => {
@@ -244,6 +321,7 @@ const GraphBoard = () => {
             setIsMousePressed(false);
             setMouseDownPos(null);
         };
+        canvasResolutionSet();
         window.addEventListener("resize", canvasResolutionSet);
         window.addEventListener("keydown", keyDown);
         window.addEventListener("keyup", keyUp);
@@ -251,7 +329,6 @@ const GraphBoard = () => {
         canvas.addEventListener("mousedown", onMouseDown);
         canvas.addEventListener("mouseup", onMouseUp);
         canvas.addEventListener("mouseout", onMouseOut);
-        canvasResolutionSet();
         return () => {
             window.removeEventListener("resize", canvasResolutionSet);
             window.removeEventListener("keydown", keyDown);
@@ -261,13 +338,11 @@ const GraphBoard = () => {
             canvas.removeEventListener("mouseup", onMouseUp);
             canvas.removeEventListener("mouseout", onMouseOut);
         };
-    }, [keyPressHandle]);
+    }, [keyPressHandle, canvasRes]);
 
 
     useEffect(() => {
-
-        //Higher canvas handling
-        
+        //Higher canvas handling        
         const canvas = canvasOverRef.current;
         if (!canvas)
             return ;
@@ -276,11 +351,11 @@ const GraphBoard = () => {
             return ;
         const clearCanvas = () => {
             ctx.reset();
-            // ctx.translate(.5, 0.5);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.strokeStyle = stroke_color;
             ctx.fillStyle = "transparent";
             ctx.fillRect(-1000000, -100000, 150000000, 150000000);
+            // movingHandling(ctx, currentOffc, screenOffc);
         };
 
         const draw = () => {
@@ -289,7 +364,7 @@ const GraphBoard = () => {
                 if (nd !== undefined) {
                     ctx.save();
                     ctx.strokeStyle = "blue";
-                    drawCirc(ctx, mousePosState, 30);
+                    drawCirc(ctx, fieldToCanvas(fieldMousePos), rad);
                     ctx.restore();
                 }
                 graph?.edges.forEach((i) => {
@@ -297,29 +372,26 @@ const GraphBoard = () => {
                         ctx.save();
                         ctx.strokeStyle  = stroke_color;
                         ctx.fillStyle  = stroke_color;
-                        let st = i.nodes[0] === nd.id ? mousePosState : 
+                        let st = i.nodes[0] === nd.id ? fieldMousePos : 
                             graph?.nodes?.find(j => j.id === i.nodes[0]).pos;
-                        let end = i.nodes[1] === nd.id ? mousePosState : 
+                        let end = i.nodes[1] === nd.id ? fieldMousePos : 
                             graph?.nodes?.find(j => j.id === i.nodes[1]).pos;
-                        // let st = i.nodes[0] === nd.id ? nd.pos : 
-                        //     graph?.nodes?.find(j => j.id === i.nodes[0]).pos;
-                        // let end = i.nodes[1] === nd.id ? nd.pos : 
-                        //     graph?.nodes?.find(j => j.id === i.nodes[1]).pos;
-                        drawEdge(ctx, st, end);
+                        drawEdge(ctx, fieldToCanvas(st), fieldToCanvas(end), rad);
                         ctx.restore();
                     }
 
                 });
             }
         };
-
         clearCanvas();
         draw();
         
-    }, [graph, mousePosState, currNode]);
+    }, [graph, fieldMousePos, currNode, screenOffc, fieldToCanvas, stroke_color]);
 
     useEffect(() => {
-        callsRef.current[5]++; 
+
+        //Underlying canvas handling
+        
         const canvas = canvasRef.current;
         if (!canvas)
             return ;
@@ -330,21 +402,23 @@ const GraphBoard = () => {
         ctx.imageSmoothingEnabled = false;
         const clearCanvas = () => {
             ctx.reset();
-            // ctx.translate(.5, 0.5);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.fillStyle = back_color;
             ctx.fillRect(-1000000, -100000, 150000000, 150000000);
             ctx.strokeStyle = stroke_color;
             ctx.fillStyle = stroke_color;
+            // movingHandling(ctx, currentOffc, screenOffc);
+
         };
 
-        const draw = () => {            
+        const draw = () => {
+            // ctx.scale(1.2, 1.2);
             graph?.nodes?.forEach((i => {
                 if (currNode === i.id) {
                     ctx.save();
-                    ctx.setLineDash([5, 5]);
+                    ctx.setLineDash([3, 3]);
                 }
-                drawCirc(ctx, i.pos, 30);
+                drawCirc(ctx, fieldToCanvas(i.pos), rad);
                 if (currNode === i.id) {
                     ctx.restore();
                 }
@@ -352,20 +426,22 @@ const GraphBoard = () => {
             graph?.edges.forEach((i) => {
                 if (i.nodes.indexOf(currNode) >= 0) {
                     ctx.save();
-                    ctx.setLineDash([5, 5]);                    
+                    ctx.setLineDash([3, 3]);
                 }
                 let st = graph?.nodes?.find(j => j.id === i.nodes[0]).pos;
                 let end = graph?.nodes?.find(j => j.id === i.nodes[1]).pos;
-                drawEdge(ctx, st, end);
+                drawEdge(ctx, fieldToCanvas(st), fieldToCanvas(end), rad);
                 if (i.nodes.indexOf(currNode) >= 0) {
                     ctx.restore();
                 }
             });
         };
-
         clearCanvas();
+        ctx.scale(scale, scale);
         draw();
-    }, [canvasRes, graph, isDarkTheme, currNode]);
+
+    }, [canvasRes, graph, isDarkTheme, currNode, fieldToCanvas,
+        back_color, screenOffc, stroke_color, scale]);
     
     return (
         <div className="logicBoard">
@@ -381,13 +457,12 @@ const GraphBoard = () => {
                 Press "c" to clear the field
               </li>
               <li>
-                Press on a node and then predd "d" to delete the node
+                Press on a node and then press "d" to delete the node
               </li>
               <li>
                 Press on consecutive nodes to draw an edge
               </li>            
-            </ul>
-            
+            </ul>            
           </div>
           <canvas ref={canvasRef} width={canvasRes[0]} height={canvasRes[1]}/>
           <canvas
@@ -396,18 +471,6 @@ const GraphBoard = () => {
             width={canvasRes[0]}
             height={canvasRes[1]}
           />
-          {/* <button style={{ */}
-          {/*     padding: "20px", */}
-          {/*     position: "fixed", */}
-          {/*     top: "0", */}
-          {/*     right: "40px", */}
-          {/*     background: "green" */}
-          {/* }} */}
-          {/*         onClick={() => { */}
-          {/*             console.log("currToll " + callsRef.current); */}
-          {/*         }} */}
-          {/* >             */}
-          {/* </button> */}
         </div>
     );
 };
