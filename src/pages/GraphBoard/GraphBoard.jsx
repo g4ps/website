809a3406201ebfde4320bdc4e9f0da894868357a
g4ps <eum4rckin@yandex.ru>
2023-07-25 23:49:38 +0000

    refactored stuff around

--- src/pages/GraphBoard/GraphBoard.jsx
+++ src/pages/GraphBoard/GraphBoard.jsx
@@ -2,120 +2,15 @@ import {useEffect, useState, useRef, useCallback, useMemo} from 'react';
 import { useSelector } from 'react-redux';
 import Slider from '../../components/Slider/Slider.jsx';
 import './GraphBoard.scss';
+import {notColliding, isTouching, detectMob,
+        sq_norm, drawCirc, drawAxis, drawFieldBox, drawEdge} from './auxFunctions.jsx';
 
 // Disclamer: vast majority of the stuff that's going
-// on here was half-assed and was done for pure fun. 
+// on here was half-assed and was done purely for fun
+
+// TODO: split this thing into several managable pieces
 
 const rad = 15;
-
-//checks whether the current position is at least rad away (heh) from any node in arr
-const notColliding = (arr, pos, rad) => {
-    for (let j = 0; j < arr.length; j++) {
-        let i = arr[j];
-        if (sq_norm([i.pos[0] - pos[0], i.pos[1] - pos[1]]) < (rad * 2) ** 2)
-            return false;
-    }
-    return true;
-};
-
-//basic check on whether the edge is touching any elements of array
-const isTouching = (edg, idArr) => {
-    return idArr.indexOf(edg[0]) >= 0 || idArr.indexOf(edg[1]) >= 0;            
-};
-
-const detectMob = () =>  {
-    //stolen from StackOverflow;
-    //if the user-agent is mobile, returns true;
-    const toMatch = [
-        /Android/i,
-        /webOS/i,
-        /iPhone/i,
-        /iPad/i,
-        /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
-    ];
-    
-    return toMatch.some((toMatchItem) => {
-        return navigator.userAgent.match(toMatchItem);
-    });
-};
-
-//Square of a eucledian norm;
-const sq_norm = (vec) => {
-    let ret = 0;
-    vec.map(i => ret += i ** 2);
-    return ret;
-};
-
-const drawCirc = (ctx, start, rad = 30) => {
-    ctx.beginPath();
-    ctx.arc(start[0], start[1], rad, 0, Math.PI * 2, true); // Outer circle
-    ctx.stroke();
-    ctx.closePath();                
-};
-
-const drawAxis = (ctx, bound, convert) => {
-    ctx.save();
-    ctx.lineWidth = 5;
-    let ox = [convert([bound[0][0], 0]), convert([bound[0][1], 0])];
-    let oy = [convert([0, bound[1][0]]), convert([0, bound[1][1]])];
-    ctx.beginPath();
-    ctx.moveTo(ox[0][0], ox[0][1]);
-    ctx.lineTo(ox[1][0], ox[1][1]);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(oy[0][0], oy[0][1]);
-    ctx.lineTo(oy[1][0], oy[1][1]);
-    ctx.stroke();
-    ctx.restore();
-}
-
-const drawFieldBox = (ctx, bound, convert) => {
-    ctx.save();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "red";
-    //Boundaries of the field
-    let lines = [
-        [convert([bound[0][0], bound[1][0]]), convert([bound[0][1], bound[1][0]])],
-        [convert([bound[0][0], bound[1][1]]), convert([bound[0][1], bound[1][1]])],
-        [convert([bound[0][0], bound[1][1]]), convert([bound[0][0], bound[1][0]])],
-        [convert([bound[0][1], bound[1][1]]), convert([bound[0][1], bound[1][0]])],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        ctx.beginPath();
-        ctx.moveTo(lines[i][0][0], lines[i][0][1]);
-        ctx.lineTo(lines[i][1][0], lines[i][1][1]);
-        ctx.stroke();
-    }
-    ctx.restore();
-};
-
-const drawEdge = (ctx, start, end, rad=30) => {
-    ctx.beginPath();
-    const vec = [end[0] - start[0], end[1] - start[1]];
-    let len = Math.sqrt(vec[0] ** 2 + vec[1] ** 2);
-    let nrm = [vec[0] / len, vec[1] / len];
-    ctx.moveTo(start[0] + nrm[0] * rad, start[1] + nrm[1] * rad);
-    let endpos = [end[0] - nrm[0] * rad, end[1] - nrm[1] * rad];
-    ctx.lineTo(endpos[0], endpos[1]);
-    const arrowLen = rad / 3;
-    //Here we implicitly use a 2D rotation matrix to draw arrows.
-    //For explanation go to wiki or any good linear algebra book
-    const cst = 0.8660254037844387;
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(endpos[0], endpos[1]);
-    ctx.lineTo(endpos[0] - (nrm[0] * cst * arrowLen) - (nrm[1] * 0.5 * arrowLen),
-               endpos[1] + (nrm[0] * 0.5 * arrowLen) - (nrm[1] * cst * arrowLen));
-    ctx.lineTo(endpos[0] - (nrm[0] * cst) * arrowLen + (nrm[1] * 0.5) * arrowLen,
-               endpos[1] - (nrm[0] * 0.5 * arrowLen) - (nrm[1] * cst * arrowLen));
-    ctx.lineTo(endpos[0], endpos[1]);
-    ctx.stroke();
-    ctx.fill();
-};
-
-
 
 const GraphBoard = () => {
 
@@ -154,6 +49,9 @@ const GraphBoard = () => {
     const ctrlDown = useRef(false);
     const shftDown = useRef(false);
 
+    //LOGIC: selection of node marker
+    const nodeMarker = useRef(false);
+
     //Unused as of now; may have some usability later
     const isDirectional = useRef(true);
     
@@ -175,6 +73,7 @@ const GraphBoard = () => {
     const [graph, setGraph] = useState(null);
 
     const [scale, setScale] = useState(1.0); //current scale
+    const [testState, setTestState] = useState(0);
 
     const [screenOffc, setScreenOffc] = useState([0, 0]); //left down corner of the canvas
 
@@ -183,6 +82,21 @@ const GraphBoard = () => {
 
     const maxScale = 8;
     const minScale = 0.3;
+
+    const buttonsArr = [
+        {
+            op: "and",            
+        },
+        {
+            op: "or",            
+        },
+        {
+            op: "not",            
+        },
+        {
+            op: "imp",            
+        },
+    ];
 
     const changeScale = useCallback((ds) => {
         // Bound for the scale; sets the value in interval [minScale, maxScale]
@@ -401,8 +315,6 @@ const GraphBoard = () => {
         setGraph(JSON.parse(JSON.stringify(graphRef.current)));
         setCurrNode(null);
     }, []); 
-
-
 
     const deleteNode = useCallback((id) => {
         graphRef.current.nodes = graphRef.current.nodes.filter(i => i.id !== id);
@@ -801,6 +713,16 @@ const GraphBoard = () => {
                  value={scale}
                />
              </div>
+             {/* <div className="labels"> */}
+             {/*   {buttonsArr.map((i, pos) => */}
+             {/*       <button onClick={() => { */}
+             {/*           nodeMarker.current = i.op; */}
+             {/*           setTestState(testState + 1); */}
+             {/*       }} key={pos}> */}
+             {/*         {i.op.toUpperCase()} */}
+             {/*       </button> */}
+             {/*   )} */}
+             {/* </div> */}
              <div className="debugInfo">
                <div>
                  {`current node ID: ${currNode === null ? "none" : currNode}`}
@@ -814,6 +736,12 @@ const GraphBoard = () => {
                <div>
                  {`graphToObj: ${JSON.stringify(graphToObj)}`}
                </div>
+               <div>
+                 {`nodeMarker: ${JSON.stringify(nodeMarker.current)}`}
+               </div>
+               <div>
+                 {`graph: ${JSON.stringify(graph)}`}
+               </div> 
              </div>
            </>
           }
---
+++ src/pages/GraphBoard/auxFunctions.jsx
@@ -0,0 +1,108 @@
+//checks whether the current position is at least rad away (heh) from any node in arr
+const notColliding = (arr, pos, rad) => {
+    for (let j = 0; j < arr.length; j++) {
+        let i = arr[j];
+        if (sq_norm([i.pos[0] - pos[0], i.pos[1] - pos[1]]) < (rad * 2) ** 2)
+            return false;
+    }
+    return true;
+};
+
+//basic check on whether the edge is touching any elements of array
+const isTouching = (edg, idArr) => {
+    return idArr.indexOf(edg[0]) >= 0 || idArr.indexOf(edg[1]) >= 0;            
+};
+
+const detectMob = () =>  {
+    //stolen from StackOverflow;
+    //if the user-agent is mobile, returns true;
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+};
+
+//Square of a eucledian norm;
+const sq_norm = (vec) => {
+    let ret = 0;
+    vec.map(i => ret += i ** 2);
+    return ret;
+};
+
+const drawCirc = (ctx, start, rad = 30) => {
+    ctx.beginPath();
+    ctx.arc(start[0], start[1], rad, 0, Math.PI * 2, true); // Outer circle
+    ctx.stroke();
+    ctx.closePath();                
+};
+
+const drawAxis = (ctx, bound, convert) => {
+    ctx.save();
+    ctx.lineWidth = 5;
+    let ox = [convert([bound[0][0], 0]), convert([bound[0][1], 0])];
+    let oy = [convert([0, bound[1][0]]), convert([0, bound[1][1]])];
+    ctx.beginPath();
+    ctx.moveTo(ox[0][0], ox[0][1]);
+    ctx.lineTo(ox[1][0], ox[1][1]);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(oy[0][0], oy[0][1]);
+    ctx.lineTo(oy[1][0], oy[1][1]);
+    ctx.stroke();
+    ctx.restore();
+}
+
+const drawFieldBox = (ctx, bound, convert) => {
+    ctx.save();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "red";
+    //Boundaries of the field
+    let lines = [
+        [convert([bound[0][0], bound[1][0]]), convert([bound[0][1], bound[1][0]])],
+        [convert([bound[0][0], bound[1][1]]), convert([bound[0][1], bound[1][1]])],
+        [convert([bound[0][0], bound[1][1]]), convert([bound[0][0], bound[1][0]])],
+        [convert([bound[0][1], bound[1][1]]), convert([bound[0][1], bound[1][0]])],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(lines[i][0][0], lines[i][0][1]);
+        ctx.lineTo(lines[i][1][0], lines[i][1][1]);
+        ctx.stroke();
+    }
+    ctx.restore();
+};
+
+const drawEdge = (ctx, start, end, rad=30) => {
+    ctx.beginPath();
+    const vec = [end[0] - start[0], end[1] - start[1]];
+    let len = Math.sqrt(vec[0] ** 2 + vec[1] ** 2);
+    let nrm = [vec[0] / len, vec[1] / len];
+    ctx.moveTo(start[0] + nrm[0] * rad, start[1] + nrm[1] * rad);
+    let endpos = [end[0] - nrm[0] * rad, end[1] - nrm[1] * rad];
+    ctx.lineTo(endpos[0], endpos[1]);
+    const arrowLen = rad / 3;
+    //Here we implicitly use a 2D rotation matrix to draw arrows.
+    //For explanation go to wiki or any good linear algebra book
+    const cst = 0.8660254037844387;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(endpos[0], endpos[1]);
+    ctx.lineTo(endpos[0] - (nrm[0] * cst * arrowLen) - (nrm[1] * 0.5 * arrowLen),
+               endpos[1] + (nrm[0] * 0.5 * arrowLen) - (nrm[1] * cst * arrowLen));
+    ctx.lineTo(endpos[0] - (nrm[0] * cst) * arrowLen + (nrm[1] * 0.5) * arrowLen,
+               endpos[1] - (nrm[0] * 0.5 * arrowLen) - (nrm[1] * cst * arrowLen));
+    ctx.lineTo(endpos[0], endpos[1]);
+    ctx.stroke();
+    ctx.fill();
+};
+
+export {notColliding, isTouching, detectMob, sq_norm, drawCirc, drawAxis, drawFieldBox, drawEdge};
